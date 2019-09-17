5dc51604e8b140ebc7fcc979e1d62eaaa040df6f
Kenta Saito <kenta.saito@sagittar.org>
2019-09-17 16:57:29 +0900

    2019-09-17 16:57:29

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ file_exists('./application/classes/super_core.php') ?
 ;
 file_exists('./application/classes/core.php') ?
 	require_once './application/classes/core.php' :
-	file_exists('./application/classes/core.php') ?
-		require_once './system/classes/core.php' :
-		class_alias('super_core', 'core')
+	class_alias('super_core', 'core')
 ;
 (new core($_SERVER))->exec_request();
