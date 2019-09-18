a26b9ea5a4dfeda72eb7e568c88febd5b3e01963
Kenta Saito <kenta.saito@sagittar.org>
2019-09-18 15:08:06 +0900

    2019-09-18 15:08:06

--- system/mails/auth/change_email.php
+++ system/mails/auth/change_email.php
@@ -2,3 +2,5 @@ We accepted a change email request
 
 Please change email from the following URL within <?php echo $this->target['token_expire_minutes']; ?> minutes.
 <?php $this->href($this->request['target'].'/change_email_verify/'.$data['id'].'/'.$data['token'], ['actor' => 'g']); ?>
+
+<?php $this->load_mail('signature', $data); ?>
