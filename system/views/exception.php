7128d662c8186f4f87ead17a48159311dfa97ac1
Kenta Saito <kenta.saito@sagittar.org>
2019-09-21 09:43:22 +0900

    2019-09-21 09:43:22

--- system/views/exception.php
+++ system/views/exception.php
@@ -11,13 +11,13 @@
   <body>
     <div class="container jumbotron">
       <h1 class="text-center"><?php $this->h($data['error_message']); ?></h1>
-<div class="card">
-  <div class="card-body">
-    <p class="card-text"><code><?php $this->h($data['debug']['exception_message']); ?></code> in <code><?php $this->h(preg_replace('#^'.getcwd().'#', '.', $data['debug']['exception_file'])); ?></code> on line <code><?php $this->h($data['debug']['exception_line']); ?></code></p>
-    <p class="card-text"><?php $this->h($data['debug']['debug_message']); ?></p>
-    <p class="card-text small text-muted text-right">To hide this message, set <code>debug</code> to <code>false</code> in <code>config.php</code>.</p>
-  </div>
-</div>
+      <div class="card">
+          <div class="card-body">
+            <p class="card-text"><code><?php $this->h($data['debug']['exception_message']); ?></code> in <code><?php $this->h(preg_replace('#^'.getcwd().'#', '.', $data['debug']['exception_file'])); ?></code> on line <code><?php $this->h($data['debug']['exception_line']); ?></code></p>
+            <p class="card-text"><?php $this->h($data['debug']['debug_message']); ?></p>
+            <p class="card-text small text-muted text-right">To hide this message, set <code>debug</code> to <code>false</code> in <code>config.php</code>.</p>
+          </div>
+        </div>
     </div>
   </body>
 </html>
