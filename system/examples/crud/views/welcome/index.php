18330bd06598b2baea6c31860e6a942f7adbc2c3
Kenta Saito <kenta.saito@sagittar.org>
2019-09-30 18:52:07 +0900

    2019-09-30 18:52:07

--- system/examples/crud/views/welcome/index.php
+++ system/examples/crud/views/welcome/index.php
@@ -1,2 +1,9 @@
 <div class="container">
+  <div class="alert alert-info">
+    If a database exception occurs, set the appropriate values in <code>./application/config.php</code> then execute a commands like following.
+    <pre>
+mysql -uroot -e 'CREATE DATABASE `pieni_examples_crud`;'
+mysql -uroot pieni_examples_crud < ./system/examples/crud/pieni_examples_crud.dump
+</pre>
+  </div>
 </div>
