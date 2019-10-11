0fcbf9a1289b21398aafb2b740ad9853fcc39bc6
Kenta Saito <kenta.saito@sagittar.org>
2019-10-11 16:31:16 +0900

    2019-10-11 16:31:16

--- system/controllers/super_crud_controller.js
+++ system/controllers/super_crud_controller.js
@@ -54,7 +54,7 @@ class super_crud_controller extends controller {
 						result.forEach(function(row){
 							const row_element = row_template.clone(true).removeClass('d-none');
 							row_element.find('[name="name"]').empty().append($('<a>').attr('href', this.href(child_name + '/view/' + row['id'])).text(row['name']));;
-							Object.keys(this.target.children[child_name].columns).forEach(function(column_name){
+							this.target.children[child_name].as_child_of[this.target.target].action_column_names.child_of.forEach(function(column_name){
 								row_element.find('[name="' + column_name + '"]').text(row[column_name]);
 							});
 							$('#' + child_name + ' table').append(row_element);
