74c6edbb165565ea4b1bfb4e7b13e6ab95180dd9
Kenta Saito <kenta.saito@sagittar.org>
2019-10-15 17:42:47 +0900

    2019-10-15 17:42:47

--- system/views/crud/edit.php
+++ system/views/crud/edit.php
@@ -1,4 +1,4 @@
-<form class="modal fade" id="<?php $this->h($target['target']); ?>_edit">
+<form class="modal fade" id="<?php $this->h($target['target']); ?>_edit" tabindex="-1">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
