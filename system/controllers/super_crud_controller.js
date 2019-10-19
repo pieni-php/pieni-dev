d9045f2b28a3675a8eaa77ddd7f7021aca143068
Kenta Saito <kenta.saito@sagittar.org>
2019-10-19 13:27:28 +0900

    2019-10-19 13:27:28

--- system/controllers/super_crud_controller.js
+++ system/controllers/super_crud_controller.js
@@ -1,2 +1,27 @@
 class super_crud_controller extends controller {
+	index()
+	{
+		$.ajax({
+			url: this.href(this.request.target, {type: 'api'}),
+			success: (result) => {
+				console.log(result);
+			},
+			error: (jqXHR) => {
+				this.show_exception_modal(JSON.parse(jqXHR.responseText));
+			},
+		});
+	}
+
+	view(id)
+	{
+		$.ajax({
+			url: this.href(this.request.target + '/view/' + id, {type: 'api'}),
+			success: (result) => {
+				console.log(result);
+			},
+			error: (jqXHR) => {
+				this.show_exception_modal(JSON.parse(jqXHR.responseText));
+			},
+		});
+	}
 }
--- system/models/super_crud_model.php
+++ system/models/super_crud_model.php
@@ -2,9 +2,11 @@
 class super_crud_model extends model {
 	public function index()
 	{
+		return $this->target;
 	}
 
 	public function view($id)
 	{
+		return $id;
 	}
 }
